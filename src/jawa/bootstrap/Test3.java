3edb7a5449c949e184fde043e45740bbe264cf79
昌堃 <421664012@qq.com>
2018-05-24 19:27:35 +0800

    chapter7 start

---
+++ src/jawa/bootstrap/Test3.java
@@ -0,0 +1,11 @@
+package jawa.bootstrap;
+
+public class Test3 {
+    int a = 2;
+    public static void main(String[] args) {
+        new Test3().a = 4;
+    }
+    public static void f(String s){
+        System.out.println(s);
+    }
+}
--- src/jawa/rtda/heap/MethodRef.java
+++ src/jawa/rtda/heap/MethodRef.java
@@ -2,6 +2,8 @@ package jawa.rtda.heap;
 
 import jawa.classfiles.constant.ConstantMethodrefInfo;
 
+import java.util.Optional;
+
 public class MethodRef extends MemberRef {
     private XMethod method;
 
@@ -20,5 +22,78 @@ public class MethodRef extends MemberRef {
 
     public void setMethod(XMethod method) {
         this.method = method;
+    }
+
+    public XMethod resolvedMethod(){
+        if(method == null)
+            resolvedMethodRef();
+        return method;
+    }
+
+    public XMethod resolvedInterfaceMethod(){
+        if(method == null)
+            resolvedInterfaceMethodRef();
+        return method;
+    }
+
+    public void resolvedInterfaceMethodRef(){
+        XClass xClass = cp.getxClass();
+        XClass xClazz = resolvedClass();
+        if(!xClazz.isInterface())
+            throw new RuntimeException("java.lang.IncompatibleClassChangeError");
+        XMethod method = lookupInterfaceMethod(xClazz,name,descriptor).orElse(null);
+        if(method == null)
+            throw new RuntimeException("java.lang.NoSuchMethodError");
+        if(!method.isAccessibleTo(xClass))
+            throw new RuntimeException("java.lang.IllegalAccessError");
+        this.method = method;
+    }
+
+    public Optional<XMethod> lookupInterfaceMethod(XClass xClass,String name,String descriptor){
+
+    }
+
+    public void resolvedMethodRef(){
+        XClass xClass = cp.getxClass();
+        XClass xClazz = resolvedClass();
+        if(xClazz.isInterface())
+            throw new RuntimeException("java.lang.IncompatibleClassChangeError");
+        XMethod method = lookupMethod(xClazz,name,descriptor).orElse(null);
+        if(method == null)
+            throw new RuntimeException("java.lang.NoSuchMethodError");
+        if(!method.isAccessibleTo(xClass))
+            throw new RuntimeException("java.lang.IllegalAccessError");
+        this.method = method;
+    }
+    public Optional<XMethod> lookupMethod(XClass xClass,String name,String descriptor){
+        XMethod method = lookupMethodInClass(xClass,name,descriptor).orElse(null);
+        if(method == null)
+            method = lookupMethodInInterfaces(xClass.getInterfaces(),name,descriptor).orElse(null);
+        return Optional.ofNullable(method);
+    }
+    public Optional<XMethod> lookupMethodInClass(XClass xClass,String name,String descriptor){
+        while(xClass.getSuperClass() != null){
+            XClass xClazz = xClass.getSuperClass();
+            for (int i = 0; i < xClazz.getMethods().size(); i++) {
+                if(name.endsWith(xClazz.getMethods().get(i).name) && descriptor.endsWith(xClazz.getMethods().get(i).descroptor)){
+                    return Optional.ofNullable(xClazz.getMethods().get(i));
+                }
+            }
+            xClass = xClazz;
+        }
+        return Optional.empty();
+    }
+
+    public Optional<XMethod> lookupMethodInInterfaces(XClass[] xClass,String name,String descriptor){
+        for (int i = 0; i < xClass.length; i++) {
+            for (int j = 0; j < xClass[i].getMethods().size(); j++) {
+                if(xClass[i].getMethods().get(j).getName().equals(name)
+                        && xClass[i].getMethods().get(j).getDescroptor().equals(descriptor)){
+                    return Optional.ofNullable(xClass[i].getMethods().get(j));
+                }
+            }
+            return lookupMethodInInterfaces(xClass[i].getInterfaces(),name,descriptor);
+        }
+        return Optional.empty();
     }
 }
